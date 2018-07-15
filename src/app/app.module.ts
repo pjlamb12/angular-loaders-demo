545a34ad1ed1fde086b71bfde66ee4165a0aaab0
Preston Lamb <preston.j.lamb@gmail.com>
2018-07-14 22:39:49 -0600

    add demo code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgLoadersModule } from 'ng-loaders';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, NavigationComponent],
+	imports: [BrowserModule, NgLoadersModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
